f9dfff0b1e5fd33b53e2a613e100717cc0e79c09
Sanya Safi <sanyasafi94@gmail.com>
2025-05-28 17:41:08 -0500

    added questions, answers, and began to code function

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+
+var questionArr = [
+    {
+        question: 'Cats are felines.',
+        answer: true
+    }
+
+    {
+        question: 'Cats like to play fetch.',
+        answer: false
+    }
+
+    {
+        question: 'Cats have whiskers.',
+        answer: true
+    } 
+
+    {
+        question: 'Cats make barking noises.',
+        answer: false
+    }
+
+    {
+        question: 'Cats are the cutest animals.',
+        answer: true
+    }
+    
+]
+
+function runQuiz (questionArr) {
+    let correctAnswers = 0;
+    let allQuestions = questionArr.length;
+    
+    for (let i =0; i < allQuestions; i++){
+        var question = questionArr[i].question;
+        var answer = questionArr[i].answer;
+        let pickedAnswer = confirm(question)
+    }
+
+}
