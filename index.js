b45447f4b47a1a60e11b4990f82226d2b7e66ec4
Sanya Safi <sanyasafi94@gmail.com>
2025-05-30 12:59:54 -0500

    adjusting the questions and adding the for loop

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 function runQuiz() {
-  var questionArr = [
+  var questionsArr = [
     {
       question: "Cats are the cutest pets",
       answer: true,
-
+    },
+    {
       question: "Cats make barking sounds",
       answer: false,
-
+    },
+    {
       question: "Cats play fetch",
       answer: false,
-
+    },
+    {
       question: "Cats have four legs and a tail",
       answer: true,
-
+    },
+    {
       question: "Cats can be indoor and outdoor pets",
       answer: true,
     },
   ];
-  for (var i = 0; i < questionArr.length; i++) {
-    console.log(i);
+
+  for (let i = 0; i < questionsArr.length; i++) {
+    var currentQuestion = questionsArr[i].question;
+    var currentAnswer = questionsArr[i].answer;
   }
 }
