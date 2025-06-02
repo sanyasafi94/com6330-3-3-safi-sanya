50f95000b27deb78fa950ffa777ba2ce45e2c1e3
Sanya Safi <sanyasafi94@gmail.com>
2025-06-01 21:17:31 -0500

    finish assignment

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
+var questionsArr = [
+  {
+    question: "Cats are the cutest pets",
+    answer: true,
+  },
+  {
+    question: "Cats make barking sounds",
+    answer: false,
+  },
+  {
+    question: "Cats play fetch",
+    answer: false,
+  },
+  {
+    question: "Cats have four legs and a tail",
+    answer: true,
+  },
+  {
+    question: "Cats can be indoor and outdoor pets",
+    answer: true,
+  },
+];
 function runQuiz() {
-  var questionsArr = [
-    {
-      question: "Cats are the cutest pets",
-      answer: true,
-    },
-    {
-      question: "Cats make barking sounds",
-      answer: false,
-    },
-    {
-      question: "Cats play fetch",
-      answer: false,
-    },
-    {
-      question: "Cats have four legs and a tail",
-      answer: true,
-    },
-    {
-      question: "Cats can be indoor and outdoor pets",
-      answer: true,
-    },
-  ];
-
   let score = 0;
   var totalQuestions = questionsArr.length;
 
@@ -36,11 +35,12 @@ function runQuiz() {
     if (userAnswer === currentAnswer) {
       score++;
     }
-
-    let percentageCorrect;
-    if (totalQuestions > 0) {
-      percentageCorrect = (score / totalQuestions) * 100;
-    }
-    let roundedPercentage = Math.ceil(percentageCorrect);
   }
+  let percentageCorrect;
+  if (totalQuestions > 0) {
+    percentageCorrect = (score / totalQuestions) * 100;
+  }
+  let roundedPercentage = Math.ceil(percentageCorrect);
+  console.log("roundedPercentage:", roundedPercentage);
+  alert("Your score is " + roundedPercentage + "%");
 }
